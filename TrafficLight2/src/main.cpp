94fd5e7ffdb20b5ca755e6d4d666d7dde85ec9cc
VovaZ <vladimir.zatuliveter@gmail.com>
2019-02-24 19:23:40 +0600

    f

---
+++ TrafficLight2/src/main.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+
+int red = 4;
+int yellow = 3;
+int green = 2;
+int beep = 5;
+
+void setup() {
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(beep, OUTPUT);
+}
+
+void SetLight(bool redSig, bool yellowSig, bool greenSig, int waitTime, int repeatCount, int repeatDelay)
+{   
+  digitalWrite(red, redSig);
+  digitalWrite(yellow, yellowSig);
+  digitalWrite(green, greenSig);
+  delay(waitTime);
+
+  for(int i=0; i<repeatCount; i++)
+  { 
+    if(redSig==1) digitalWrite(red, !redSig);
+    if(yellowSig==1) digitalWrite(yellow, !yellowSig);
+    if(greenSig==1) digitalWrite(green, !greenSig);
+    
+    digitalWrite(beep, 0); 
+
+    delay(repeatDelay / 2);
+
+    digitalWrite(red, redSig);
+    digitalWrite(yellow, yellowSig);
+    digitalWrite(green, greenSig);
+    digitalWrite(beep, 1);
+
+    delay(repeatDelay / 2);
+  }
+  
+  digitalWrite(beep, 0);
+}
+
+void loop() {
+  SetLight(0, 0, 1, 3000, 7, 300);
+  
+  SetLight(0, 1, 0, 1000, 0, 0);
+  
+  SetLight(1, 0, 0, 3000, 7, 300);
+  
+  SetLight(0, 1, 0, 3000, 0, 0);
+}
+
+
