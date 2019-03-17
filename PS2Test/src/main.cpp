816359764e582efe5ce80cd127cb1c8ec8496ac6
VovaZ <vladimir.zatuliveter@gmail.com>
2019-03-17 13:18:43 +0600

    one motor in working state

--- PS2Test/src/main.cpp
+++ PS2Test/src/main.cpp
@@ -3,16 +3,24 @@
 
 Controller controller;
 
-void setup(){
+byte LeftMotorAPin = 4;
+byte LeftMotorBPin = 5;
+byte LeftMotorSpeedPin = 3;
+
+void setup()
+{
+  pinMode (LeftMotorAPin, OUTPUT); 
+  pinMode (LeftMotorBPin, OUTPUT);
+  pinMode (LeftMotorSpeedPin, OUTPUT);
+
   Serial.begin(9600);
-  delay(500); 
+  delay(500);
+  
   controller.Setup(); 
 }
 
-void loop(){
-   
-   ControllerState state = controller.Read();
-
+void DispalyController(ControllerState state)
+{  
    Serial.print("LX=");
    Serial.print(state.LX, DEC);
 
@@ -26,6 +34,25 @@ void loop(){
    Serial.print(state.RY, DEC);
    
    Serial.println('.');
-   
-   delay(50);     
+}
+
+void loop()
+{
+  ControllerState state = controller.Read();
+  DispalyController(state);
+
+  if (state.LY > 128) {
+      digitalWrite(LeftMotorAPin, 0);
+      digitalWrite(LeftMotorBPin, 1);
+      analogWrite(LeftMotorSpeedPin, (state.LY - 128) * 2);
+  } else if (state.LY < 128) {
+      digitalWrite(LeftMotorAPin, 1);
+      digitalWrite(LeftMotorBPin, 0);
+      analogWrite(LeftMotorSpeedPin, 250 - state.LY * 2);      
+  } else {
+    digitalWrite(LeftMotorAPin, 0);
+    digitalWrite(LeftMotorBPin, 0);      
+  }
+
+  delay(50);
 }
