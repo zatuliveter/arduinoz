e104738a6c79eaaaf24b5582e9f11eb2d7b69ae0
VovaZ <vladimir.zatuliveter@gmail.com>
2019-03-17 08:03:23 +0600

    controller class and its usage

---
+++ PS2Test/src/controller.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <PS2X_lib.h>
+
+class ControllerState {
+public:
+    byte LY;
+    byte LX;
+    byte RY;
+    byte RX;
+};
+
+class Controller {
+private:
+    PS2X ps2 = PS2X();
+public:
+    void Setup()
+    {
+        //setup pins and settings:  GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
+        byte error = ps2.config_gamepad(13, 11, 10, 12, true, true);
+        byte type = ps2.readType();
+        Serial.print("error:");
+        Serial.println(error);
+        Serial.print("type:");
+        Serial.println(type);
+    }
+    
+    ControllerState Read()
+    {
+        ps2.read_gamepad(false, true);
+
+        ControllerState state;
+        state.LY = ps2.Analog(PSS_LY);
+        state.LX = ps2.Analog(PSS_LX);
+        state.RY = ps2.Analog(PSS_RY);
+        state.RX = ps2.Analog(PSS_RX);
+
+        return state;
+    } 
+};
