a2cdd3005bd48d5f6d99b6fcc927ca76a4688cf8
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2020-03-21 22:57:11 +0600

    перешли на arduino nano

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,68 +1,77 @@
-#include <AFMotor.h>
 #include <Servo.h>
 #include <NewPing.h>
 
-AF_DCMotor motor(2);
 Servo servo1;
-NewPing sonar1(A1 /*Trig*/, A0 /*Echo*/, 70 /*max distance*/);
-NewPing sonar2(A2 /*Trig*/, A3 /*Echo*/, 70 /*max distance*/);
+NewPing FrontSonar(2 /*Trig*/, 3 /*Echo*/, 70 /*max distance*/);
+NewPing RearSonar(5 /*Trig*/, 4 /*Echo*/, 70 /*max distance*/);
 
 int Left = 105;
 int Right = 40;
 
+int MotorForward = 12;
+int MotorBack = 13;
+int MotorPWM = 11;
+
 void setup()
-{  
+{
   //Serial.begin(9600);
-  
-  pinMode(A3, INPUT);
-  pinMode(A2, OUTPUT); 
-  
-  pinMode(A0, INPUT);
-  pinMode(A1, OUTPUT);
-  
   servo1.attach(9);
+
+  pinMode(MotorForward, OUTPUT);
+  pinMode(MotorBack, OUTPUT);
+  pinMode(MotorPWM, OUTPUT);
+}
+
+void Motor(int val) {
+  if (val > 0) {
+    digitalWrite(MotorForward, 1);
+    digitalWrite(MotorBack, 0);
+    analogWrite(MotorPWM, val); 
+  }
+  else if (val < 0) {
+    digitalWrite(MotorForward, 0);
+    digitalWrite(MotorBack, 1);
+    analogWrite(MotorPWM, -val);
+  }
+  else {
+    digitalWrite(MotorForward, 0);
+    digitalWrite(MotorBack, 0);
+    analogWrite(MotorPWM, 0);
+  }
 }
 
 void loop()
 {
-  int s1 = sonar1.ping_cm();
-  int s2 = sonar1.ping_cm();
+  int frontSonarValue = FrontSonar.ping_cm();
+  int rearSonarValue = RearSonar.ping_cm();
   //Serial.println(s);
-  
-  if (s1 > 50 || s1 == 0)
-  { 
-    //вперёд   
-    motor.run(RELEASE);
-    motor.run(FORWARD);
-    motor.setSpeed(210);
+
+  if (frontSonarValue > 50 || frontSonarValue == 0)
+  {
+    //вперёд
+    Motor(210);
     servo1.write(67);
   }
   else
   {
     //назад
-    motor.run(BACKWARD);  
-    motor.setSpeed(210); 
+    Motor(-210);
     delay(800);
-    
+
     if (random(0, 2) == 0)
       servo1.write(Left);
     else
       servo1.write(Right);
 
-    if (s2 < 20 && s2 != 0)
+    if (rearSonarValue < 20 && rearSonarValue != 0)
     {
-      motor.run(FORWARD);
-      motor.setSpeed(210);
+      Motor(210);
+      servo1.write(105);
       delay(800);
-      
-      if (random(0, 2) == 0)
-        servo1.write(Left);
-      else
-        servo1.write(Right);
     }
-      
+
     delay(800);
   }
-   
+
   delay(50);
 }
