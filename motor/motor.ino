332f64b9737afef6c52c01b436c6ecab881e9cfc
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2020-03-22 14:39:08 +0600

    добавлена поддержка Bluetoot

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,22 +1,27 @@
 #include <Servo.h>
 #include <NewPing.h>
+#include <SoftwareSerial.h>
 
 Servo servo1;
 NewPing FrontSonar(2 /*Trig*/, 3 /*Echo*/, 70 /*max distance*/);
 NewPing RearSonar(5 /*Trig*/, 4 /*Echo*/, 70 /*max distance*/);
+SoftwareSerial Bluetooth(6, 7); // RX, TX
 
-int Left = 105;
-int Right = 40;
-
+int Right = 105;
+int Left = 40;
+int Center = 67;
+    
 int MotorForward = 12;
 int MotorBack = 13;
 int MotorPWM = 11;
 
 void setup()
 {
-  //Serial.begin(9600);
+  Serial.begin(9600);
   servo1.attach(9);
 
+  Bluetooth.begin(9600);
+  
   pinMode(MotorForward, OUTPUT);
   pinMode(MotorBack, OUTPUT);
   pinMode(MotorPWM, OUTPUT);
@@ -41,6 +46,48 @@ void Motor(int val) {
 }
 
 void loop()
+{
+  bluetooth();
+  //sonar();
+}
+
+void bluetooth()
+{  
+  if (Bluetooth.available())
+  {
+    String val = Bluetooth.readString();
+    Serial.print(val);
+    
+    val = val.substring(0, 1);
+    
+    if (val == "f") { 
+      servo1.write(Center);
+      Motor(210); 
+    }
+    
+    if (val == "b") { 
+      servo1.write(Center);
+      Motor(-210);
+    } 
+    
+    if (val == "l") { 
+      servo1.write(Left);
+      Motor(210); 
+    }  
+    
+    if (val == "r") { 
+      servo1.write(Right);
+      Motor(210); 
+    }
+    
+    delay(2000); 
+  }
+
+  servo1.write(Center);
+  Motor(0); 
+}
+
+void sonar()
 {
   int frontSonarValue = FrontSonar.ping_cm();
   int rearSonarValue = RearSonar.ping_cm();
