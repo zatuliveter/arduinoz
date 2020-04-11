9fe66cfbb5f657208bc41127c4cc59bc0b9903e1
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2020-04-11 13:04:47 +0600

    Ambilight.ino for tv (current version, arduino nano)

---
+++ Ambilight/Ambilight.ino
@@ -0,0 +1,95 @@
+
+//----------------------НАСТРОЙКИ-----------------------
+#define NUM_LEDS 228         // число светодиодов в ленте
+#define DI_PIN 13            // пин, к которому подключена лента
+
+#define start_flashes 1      // проверка цветов при запуске (1 - включить, 0 - выключить)
+
+#define max_bright 180       // максимальная яркость (0 - 255)
+#define min_bright 50        // минимальная яркость (0 - 255)
+
+//----------------------НАСТРОЙКИ-----------------------
+
+int new_bright, new_bright_f;
+unsigned long bright_timer;
+
+#define serialRate 115200  // скорость связи с ПК
+uint8_t prefix[] = {'A', 'd', 'a'}, hi, lo, chk, i;  // кодовое слово Ada для связи
+#include <FastLED.h>
+CRGB leds[NUM_LEDS];  // создаём ленту
+
+void ledTest(byte r, byte g, byte b)
+{
+  byte st = 3;
+  byte i;
+  for (i = 0; i < 255; i=i+st)
+  {
+    LEDS.showColor(CRGB(i * r, i * g, i * b));
+  }
+  for (i = 255; i > 0; i=i-st)
+  {
+    LEDS.showColor(CRGB(i * r, i * g, i * b));
+  }
+}
+
+void setup()
+{
+  FastLED.addLeds<WS2812, DI_PIN, GRB>(leds, NUM_LEDS);  // инициализация светодиодов
+  
+  // тест ленты в начале
+  if (start_flashes) {
+    ledTest(1, 0, 0);
+    ledTest(0, 1, 0);
+    ledTest(0, 0, 1);
+  }
+
+  Serial.begin(serialRate);
+  Serial.print("Ada\n");     // Связаться с компом
+
+  memset(leds, 0, NUM_LEDS * sizeof(struct CRGB));
+}
+
+void initConnect() {
+  
+  for (i = 0; i < sizeof prefix; ++i) {
+waitLoop: while (!Serial.available()) ;;
+    if (prefix[i] == Serial.read()) continue;
+    i = 0;
+    goto waitLoop;
+  }
+
+  while (!Serial.available());
+  hi = Serial.read();
+  
+  while (!Serial.available());
+  lo = Serial.read();
+  
+  while (!Serial.available());
+  chk = Serial.read();
+  
+  if (chk != (hi ^ lo ^ 0x55))
+  {
+    i = 0;
+    goto waitLoop;
+  }
+  
+}
+
+void loop() {
+  initConnect();
+  
+  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+    
+    // читаем данные для каждого цвета
+    while (!Serial.available());    
+    leds[i].r = Serial.read();
+    
+    while (!Serial.available());
+    leds[i].g = Serial.read();
+    
+    while (!Serial.available());    
+    leds[i].b = Serial.read();
+  }
+  
+  FastLED.show();  // записываем цвета в ленту
+}
