62f40761bbfecc6de1a3aaaab4303026ae9112b6
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2020-05-02 15:36:21 +0600

    подстроились под полурабочий сервопривод.

--- Trashcan/Trashcan.ino
+++ Trashcan/Trashcan.ino
@@ -3,7 +3,7 @@
 #include <NewPing.h>
 
 Servo myservo;
-NewPing sonar(3 /*TRIGGER_PIN*/, 2 /*ECHO_PIN*/, 50 /*MAX_DISTANCE*/);
+NewPing sonar(3 /*TRIGGER_PIN*/, 4 /*ECHO_PIN*/, 50 /*MAX_DISTANCE*/);
 
 void setup() {                
   myservo.attach(9);
@@ -14,11 +14,11 @@ void loop()
   int s = sonar.ping_cm();
   if (s > 0 and s < 30) 
   {
-    myservo.write(25);
+    myservo.write(0);
     delay(2500);  
   }
   else {
-    myservo.write(130);
+    myservo.write(75);
     delay(20);  
   }
 }
